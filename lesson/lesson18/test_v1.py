d80bd14b7b8ce73ec2f04f39b0c31d8fc90ba0f6
AraRen <jackyccz860511@gmail.com>
2023-10-17 15:39:37 +0800

    testfail

--- lesson/lesson18/test_v1.py
+++ lesson/lesson18/test_v1.py
@@ -3,17 +3,9 @@ from tkinter import ttk
 from tkinter import filedialog
 import csv
 
-
-
-    
-def open_csv_file():
-    file_path = filedialog.askopenfilename(title="Open CSV File", filetypes=[("CSV files", "*.csv")])
-    if file_path:
-        display_csv_data(file_path)
-
 def display_csv_data(file_path):
     try:
-        with open(file_path, 'r', newline='') as file:
+        with open('台積電.csv', 'r', newline='') as file:
             csv_reader = csv.reader(file)
             header = next(csv_reader)  # Read the header row
             tree.delete(*tree.get_children())  # Clear the current data
@@ -33,9 +25,6 @@ def display_csv_data(file_path):
 
 root = tk.Tk()
 root.title("CSV File Viewer")
-
-open_button = tk.Button(root, text="Open CSV File", command=open_csv_file)
-open_button.pack(padx=20, pady=10)
 
 tree = ttk.Treeview(root, show="headings")
 tree.pack(padx=20, pady=20, fill="both", expand=True)
