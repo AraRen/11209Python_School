418c6e4f8ce959aafa8879950585670ecf514bb7
AraRen <jackyccz860511@gmail.com>
2023-11-01 15:06:11 +0800

    modify

--- lesson/youbike/index.py
+++ lesson/youbike/index.py
@@ -1,56 +1,45 @@
 import tkinter as tk
 from tkinter import ttk
+from youbikeTreeView import YoubikeTreeView
 from tkinter import messagebox
 from threading import Timer
-from youbikeTreeView import YoubikeTreeView
 import datasource
 
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        
         try:
             datasource.updata_sqlite_data()
         except Exception:
-            messagebox.showerror('錯誤','網路錯誤\n將關閉應用程式\n請稍後再試')
+            messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
             self.destroy()
         
-        #print(datasource.last_datetime_data())
-        topFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1,width=300,height=200)
-        tk.Label(topFrame,text="台北市Youbike即時資料",font=("arial",20),bg="#333333",fg="#FFFFFF").pack(padx=20,pady=20)
+        
+        #print(datasource.lastest_datetime_data())
+        topFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1)
+        tk.Label(topFrame,text="台北市youbike及時資料",font=("arial", 20), bg="#333333", fg='#ffffff',padx=10,pady=10).pack(padx=20,pady=20)
         topFrame.pack(pady=30)
 
         bottomFrame = tk.Frame(self)
-
-        self.youbikeTreeView = YoubikeTreeView(bottomFrame,show='headings',columns=('sna','sarea','mday','ar','tot','sbi','bemp'))
+        
+        self.youbikeTreeView = YoubikeTreeView(bottomFrame,show="headings",columns=('sna','mday','sarea','ar','tot','sbi','bemp'))
         self.youbikeTreeView.pack()
         bottomFrame.pack(pady=30)
 
-        lastest_data = datasource.last_datetime_data()
-        self.youbikeTreeView.update_content(lastest_data)
-
-
-
-t = None
-def main():
-    def on_closing():
-        print('視窗關閉')
-        t.cancel()
-        window.destroy()
-
-    def updata_data()->None:
+def main():    
+    def update_data(w:Window)->None:
         datasource.updata_sqlite_data()
-        global t
-        t = Timer(20,updata_data)
-        t.start()
-
+        lastest_data = datasource.lastest_datetime_data()
+        w.youbikeTreeView.update_content(lastest_data)
+        window.after(3*60*1000,update_data,w) #每隔3分鐘
+          
     window = Window()
     window.title('台北市youbike2.0')
-    window.geometry('600x300')
+    #window.geometry('600x300')
     window.resizable(width=False,height=False)
-    updata_data()
-    window.protocol("WM_DELETE_WINDOW",on_closing)
+    update_data(window)          
     window.mainloop()
-
 
 if __name__ == '__main__':
     main()
