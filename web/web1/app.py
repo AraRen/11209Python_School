57219ed7e0316f69b4b80ae8f08e315465d2cf42
AraRen <jackyccz860511@gmail.com>
2023-11-20 06:56:50 +0000

    mod

--- web/web1/app.py
+++ web/web1/app.py
@@ -1,37 +1,19 @@
 from flask import Flask,url_for,render_template
 from markupsafe import escape
-
+import random
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     name = "AraRen"
-    age = 26
-    return render_template('index.html',name=name,age=age)
+    dataFrame = get_dataFrame()
+    return render_template('index.html',name=name,dataFrame=dataFrame)
 
-@app.route("/hello")
-def hello():
-    return "hello world"
+def get_dataFrame()->pd.DataFrame:
+    data = [['AraRen',87,87,87],
+            ['AAAAAA',78,78,78],
+            ['BBBBBB',87,78,87]]
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {username}'
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id*5}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {subpath}'
-
-@app.route('/url')
-def url():
-    print(url_for('hello'))
-    print(url_for('show_user_profile',username="CBA"))
-    print(url_for('static',filename="css/style.css"))
-    return "ABC"
+    return pd.DataFrame(data,columns=["姓名","國文","英文","數學"])
