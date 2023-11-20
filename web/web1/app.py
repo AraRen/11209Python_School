bee798ccc3d83099ecf8dfd583aafa45edb57ae8
AraRen <jackyccz860511@gmail.com>
2023-11-20 09:06:12 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -1,21 +1,25 @@
 from flask import Flask,url_for,render_template
-from markupsafe import escape
-import random
 import pandas as pd
 from auth import auth
+from bs import bootstrap
+
 
 app = Flask(__name__)
 app.register_blueprint(auth.bp)
+app.register_blueprint(bootstrap.bp)
 
 @app.route('/')
 def index():
-    name = "AraRen"
+    name = "徐國堂"
     dataFrame = get_dataFrame()
     return render_template('index.html',name=name,dataFrame=dataFrame)
 
 def get_dataFrame()->pd.DataFrame:
-    data = [['AraRen',87,87,87],
-            ['AAAAAA',78,78,78],
-            ['BBBBBB',87,78,87]]
-
+    data = [['徐國堂',67,92,85],
+            ['王XX',78,97,65],
+            ['李XX',92, 79, 85]]
     return pd.DataFrame(data,columns=["姓名","國文","英文","數學"])
+
+@app.route('/aaa')
+def aaa():
+    return render_template('aaa.html')  
