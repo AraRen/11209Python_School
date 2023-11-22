7e049fc7dc08ebd9d5f46f8cf2daf1e41e3e2abd
AraRen <jackyccz860511@gmail.com>
2023-11-22 03:35:33 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -10,14 +10,14 @@ app.register_blueprint(bootstrap.bp)
 
 @app.route('/')
 def index():
-    name = "徐國堂"
+    name = "REN"
     dataFrame = get_dataFrame()
     return render_template('index.html',name=name,dataFrame=dataFrame)
 
 def get_dataFrame()->pd.DataFrame:
-    data = [['徐國堂',67,92,85],
-            ['王XX',78,97,65],
-            ['李XX',92, 79, 85]]
+    data = [['REN',67,92,85],
+            ['Jacky Chen',78,97,65],
+            ['Monica Chen',92, 79, 85]]
     return pd.DataFrame(data,columns=["姓名","國文","英文","數學"])
 
 @app.route('/aaa')
